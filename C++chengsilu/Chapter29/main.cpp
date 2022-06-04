c9ceafa963ab473e1c2489b2fdb65b91677bfd41
lige <2305383871@qq.com>
2022-06-04 14:01:56 +0800

    create chapter 29

--- C++chengsilu/Chapter29/main.cpp
+++ C++chengsilu/Chapter29/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+class base
+{
+    public:
+        virtual ~base(){};
+        virtual void a() = 0;
+};
+
+class child: public base{
+    public:
+        child(){}
+        ~child(){}
+        void a(){std::cout << "a\n";}
+};
+
+class A
+{
+    
+};
+
+base & operator << (base &w, const A & v)
+{
+    w.a();
+    return w;
+}
+
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    A b;
+    child c;
+    c << b;
 }
