a42f6a17b2eb7843cbb7c6d549c8df3cc87361b2
lige <2305383871@qq.com>
2022-06-04 12:32:15 +0800

    readd

--- C++chengsilu/Chapter27/cluster_class.hpp
+++ C++chengsilu/Chapter27/cluster_class.hpp
@@ -1,5 +1,58 @@
 #ifndef _CLUSTER_CLASS_
 #define _CLUSTER_CLASS_
-
 #include <iostream>
+
+class Clusteritem;
+
+class Cluster
+{
+    friend class Clusteritem;
+    public:
+        Cluster():head(0){};
+        ~Cluster();
+    private:
+        Clusteritem* head;
+        Cluster(const Cluster&);
+        Cluster& operator=(const Cluster&);
+};
+
+static Cluster *cp;
+
+class Clusteritem
+{
+    friend class Cluster;
+    public:
+        void* operator new(size_t n, Cluster& c)    
+        {
+            std::cout <<n <<'\n';
+            cp = &c;
+            return ::operator new(n);
+        };
+        Clusteritem()
+        {
+            next = cp->head;
+            cp->head = this;
+        };
+    protected:
+        virtual ~Clusteritem(){};
+    private:
+        Clusteritem* next;
+        void* operator new(size_t)
+        {
+            std::abort();
+            return 0;
+        };
+        Clusteritem(const Clusteritem&);
+        Clusteritem& operator=(const Clusteritem&);
+};
+
+Cluster::~Cluster()
+        {
+            while(head)
+            {
+                Clusteritem* next = head->next;
+                delete head;
+                head = next;
+            }
+        };
 #endif
--- C++chengsilu/Chapter27/main.cpp
+++ C++chengsilu/Chapter27/main.cpp
@@ -1,5 +1,19 @@
 #include "cluster_class.hpp"
 
+
+class A:public Clusteritem
+{
+    public:
+        A():val(0){}
+        A(int v):val(v){}
+    private:
+        int val;
+};
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    Cluster c;
+    A* var = new(c) A;
+    A* v = new(c) A;
+    std::cout << sizeof(A)<<'\n';
+
 }
