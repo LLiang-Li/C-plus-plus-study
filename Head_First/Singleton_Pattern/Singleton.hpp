ca76cdc38f152d82950ff90b4cea2a5dbe22dfbd
lige <2305383871@qq.com>
2022-06-11 20:48:02 +0800

    Singleton Pattern

---
+++ Head_First/Singleton_Pattern/Singleton.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+class Singleton
+{
+    private:
+        static Singleton * uniqueInstance;
+
+        Singleton(){};
+
+    public:
+        static Singleton* getInstance()
+        {
+            
+            if (!Singleton::uniqueInstance)
+            {
+                std::cout << "create\n";
+                Singleton::uniqueInstance = new Singleton;
+            }
+            return Singleton::uniqueInstance;
+        }
+};
+
+ Singleton * Singleton::uniqueInstance = 0;
---
+++ Head_First/Singleton_Pattern/main.cpp
@@ -0,0 +1,6 @@
+#include "Singleton.hpp"
+
+int main(int, char**) {
+    Singleton* n = Singleton::getInstance();
+    n = Singleton::getInstance();
+}
