5a9cae4530badf373e1a699bda22f0d49f04293e
lige <2305383871@qq.com>
2022-06-19 10:10:35 +0800

    .

--- Head_First/Tempalte_Pattern/Template_pattern.hpp
+++ Head_First/Tempalte_Pattern/Template_pattern.hpp
@@ -1,2 +1,52 @@
 #pragma once
 #include<iostream>
+#include <string>
+
+class Duck {
+	std::string name;
+	int weight;
+  
+	public:
+
+    Duck(std::string, int weight) {
+		this->name = name;
+		this->weight = weight;
+	}
+ 
+	operator std::string() {
+		return name + " weighs " + std::to_string(this->weight);
+	}
+ 
+ 
+  
+	int compareTo(Duck* object) {
+ 
+		Duck* otherDuck = object;
+  
+		if (this->weight <= otherDuck->weight) {
+			return 0;
+		} else  {
+			return 1;}
+		// } else { // this.weight > otherDuck.weight
+		// 	return 1;
+		// }
+	}
+};
+
+void sort(Duck* start, int low, int high)
+{
+    Duck temp("temp",0);
+    for (int  i = low; i < high; i++)
+    {
+        for (int j = high - 1; j >i; j--)
+        {
+            if (start[j].compareTo(&start[j - 1]) )
+            {
+                temp = start[j - 1];
+                start[j - 1] = start[j];
+                start[j] = temp;
+            }
+        }
+    }
+        
+}
--- Head_First/Tempalte_Pattern/main.cpp
+++ Head_First/Tempalte_Pattern/main.cpp
@@ -1,5 +1,28 @@
 #include "Template_pattern.hpp"
 
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+   Duck* duck = new Duck[6]{
+    Duck("Daffy", 8),
+    Duck("Dewwy", 2),
+    Duck("Howard", 7),
+    Duck("Louie", 2),
+    Duck("Donald", 10),
+    Duck("Huey", 2),
+   };
+
+    for (int i = 0; i < 6; i++)
+        std::cout << std::string(duck[i])<<"\n";
+
+    std::cout <<"----------------------------\n";
+    
+    sort(duck, 0, 6);
+
+    std::cout <<"----------------------------\n";
+    for (int i = 0; i < 6; i++)
+        std::cout << std::string(duck[i])<<"\n";
+
+    delete [] duck;
+    
+
 }
