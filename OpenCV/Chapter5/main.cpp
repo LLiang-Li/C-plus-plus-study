826c15a266d3eed322084244536c7f5b3ff092e4
lige <2929250918@qq.com>
2023-03-27 16:18:40 +0800

    Chapter5 In this, there are many functions for calculation of matrixes, for example, inv, trace, solver and pow.

---
+++ OpenCV/Chapter5/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<Log.h>
+
+int main(int argc, char** argv)
+{
+  //各种函数
+  /*
+  余弦变换
+  cv::dct()
+  矩阵的傅里叶变换
+  cv::dft()
+  方阵的特征值和特征向量
+  cv::eigen()
+  求线性方程的解
+  cv::solve()
+  矩阵的迹
+  cv::trace()
+  */
+
+  return 0;
+}
