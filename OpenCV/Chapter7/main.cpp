8ba30a62f5c4f03865848fc4a4a516fc5b9f7913
lige <2929250918@qq.com>
2023-03-28 14:38:11 +0800

    Chapter 7 There are three functions of PCA, SVD and RNG.

---
+++ OpenCV/Chapter7/main.cpp
@@ -0,0 +1,13 @@
+#include<Log.h>
+#include<opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+  //奇异值分解
+  cv::SVD s;
+  //主成分分析
+  cv::PCA p;
+  //随机数发生器
+  cv::RNG r;
+  return 0;
+}
--- OpenCV/common/Log.h
+++ OpenCV/common/Log.h
@@ -1,5 +1,8 @@
 #ifndef _MY_LOG_
 #define _MY_LOG_
+  #include<iostream>
+  #include<vector>
+  #include<string>
   #define LOG_TAG(TAG, FORMAT, ...) ::fprintf(stdout, "[" TAG "](%s:%s:%d): \n" FORMAT "\r\n", __TIME__, __FILE__, __LINE__, ##__VA_ARGS__)
   #define LOG(FORMAT, ...) ::fprintf(stdout, FORMAT , ##__VA_ARGS__)
   #define LOG_INFO(FORMAT, ...) LOG_TAG("INFO", FORMAT, ##__VA_ARGS__)
